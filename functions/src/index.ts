60a823be2ec2161cf9fc16befd827b4fde842c04
Sawyer Hollenshead <git@sawyerh.com>
2020-05-25 12:44:55 -0400

    Delete tweets older than 2 weeks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
 import * as functions from "firebase-functions";
+import { DateTime } from "luxon";
 import admin = require("firebase-admin");
 import { getTweetsThatIncludeLinks } from "./services/twitter";
+import { writeLog } from "./services/logger";
 
+/**
+ * Setup our app and database
+ */
 admin.initializeApp();
-
 const db = admin.firestore();
-const tweetsCollectionName = "tweets";
+const collection = db.collection("tweets");
 
 /**
  * Get the latest tweets that include an external link
@@ -14,8 +18,7 @@ export const getTweets = functions.pubsub
   .schedule("every 15 minutes")
   .onRun(async () => {
     // Get the latest tweet so we can query only tweets posted after it
-    const existingTweets = await db
-      .collection(tweetsCollectionName)
+    const existingTweets = await collection
       .orderBy("created_at_timestamp", "desc")
       .limit(1)
       .select("id_str")
@@ -29,8 +32,31 @@ export const getTweets = functions.pubsub
 
     // Save the latest tweets to our database
     await Promise.all(
-      tweets.map((tweet) =>
-        db.collection(tweetsCollectionName).doc(tweet.id_str).set(tweet)
-      )
+      tweets.map((tweet) => collection.doc(tweet.id_str).set(tweet))
+    );
+  });
+
+/**
+ * Remove any tweets older than 2 weeks so that we don't accrue millions of tweets
+ */
+export const removeOldTweets = functions.pubsub
+  .schedule("every 14 days")
+  .onRun(async () => {
+    const twoWeeksAgo = DateTime.local().minus({ days: 14 });
+    const tweets = await collection
+      .where("created_at_timestamp", "<", twoWeeksAgo.toJSDate())
+      .get();
+
+    writeLog(
+      "removeOldTweets",
+      `Found ${
+        tweets.docs.length
+      } that are older than ${twoWeeksAgo.toISO()} to delete`
+    );
+
+    if (tweets.empty) return;
+
+    await Promise.all(
+      tweets.docs.map((doc) => collection.doc(doc.id).delete())
     );
   });
